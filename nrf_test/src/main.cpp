a76d09bc33505806a41cb5a1243401d106a7c295
frans <frans.b@gmail.com>
2015-07-04 18:48:58 +0200

    added soil moisture measurer

--- nrf_test/src/main.cpp
+++ nrf_test/src/main.cpp
@@ -26,6 +26,7 @@
 // Singleton instance of the radio driver
 RH_NRF24 driver;
 // RH_NRF24 driver(8, 7);   // For RFM73 on Anarduino Mini
+int soilPin1 = 0;
 
 // Class to manage message delivery and receipt, using the driver declared above
 RHReliableDatagram manager(driver, CLIENT_ADDRESS);
@@ -38,13 +39,15 @@ void setup()
   // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
 }
 
-uint8_t data[] = "Hello World!";
+//uint8_t data[] = "Hello World!";
 // Dont put this on the stack:
 uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
 
 void loop()
 {
   Serial.println("Sending to nrf24_reliable_datagram_server");
+  int soilVal = analogRead(soilPin1);
+  uint8_t data[2] = {soilVal, (soilVal >> 8)};
 
   // Send a message to manager_server
   if (manager.sendtoWait(data, sizeof(data), SERVER_ADDRESS))
@@ -68,9 +71,6 @@ void loop()
     Serial.println("sendtoWait failed");
   delay(500);
 }
-
-
-
 
 int main() {
 	init();
